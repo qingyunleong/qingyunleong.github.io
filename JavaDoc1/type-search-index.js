d972540d507c83fd5af7598cca5038c595b0aebf
Qingyun <73087963+qingyunleong@users.noreply.github.com>
2023-01-20 21:51:22 +0800

    Add files via upload

---
+++ JavaDoc1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"App"},{"p":"my.uum","l":"DatabaseManager"},{"p":"my.uum","l":"STIW3054_bugbug_bot"}];updateSearchResults();
